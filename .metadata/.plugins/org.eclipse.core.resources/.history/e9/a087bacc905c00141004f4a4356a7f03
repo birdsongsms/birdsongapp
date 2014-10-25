76f47f52995ec19f308e4612ca2d7a476743de14
Peter Jacobson <peterjacobson37@gmail.com>
2014-10-25 22:56:12 +0100

    trying template from stack exchange

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e9/a087bacc905c00141004f4a4356a7f03
@@ -0,0 +1,53 @@
+package com.example.birdsong;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentReceiver;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.gsm.SmsMessage;
+import android.util.Log;
+ 
+public class SMSReceiver extends IntentReceiver {
+        /** TAG used for Debug-Logging */
+        private static final String LOG_TAG = "SMSReceiver";
+ 
+        /** The Action fired by the Android-System when a SMS was received.
+         * We are using the Default Package-Visibility */
+        private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+        // @Override
+        public void onReceiveIntent(Context context, Intent intent) {
+                if (intent.getAction().equals(ACTION)) {
+                        // if(message starts with SMStretcher recognize BYTE)
+                        StringBuilder sb = new StringBuilder();
+                       
+                        /* The SMS-Messages are 'hiding' within the extras of the Intent. */
+                        Bundle bundle = intent.getExtras();
+                        if (bundle != null) {
+                                /* Get all messages contained in the Intent*/
+                                SmsMessage[] messages =
+                                        Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                               
+                                /* Feed the StringBuilder with all Messages found. */
+                                for (SmsMessage currentMessage : messages){
+                                        sb.append("Received compressed SMSnFrom: ");
+                                        /* Sender-Number */
+                                        sb.append(currentMessage.getDisplayOriginatingAddress());
+                                        sb.append("n----Message----n");
+                                        /* Actual Message-Content */
+                                        sb.append(currentMessage.getDisplayMessageBody());
+                                }
+                        }
+                        /* Logger Debug-Output */
+                        Log.i(LOG_TAG, "[SMSApp] onReceiveIntent: " + sb);
+ 
+                        /* Show the Notification containing the Message. */
+                        Toast.makeText(context, sb.toString(), Toast.LENGTH_LONG).show();
+                        /* Start the Main-Activity */
+                        Intent i = new Intent(context, SMSActivity.class);
+                        i.setLaunchFlags(Intent.NEW_TASK_LAUNCH);
+                        context.startActivity(i);
+                }
+        }
+}
--- birdsong/src/com/example/birdsong/SMSReceiver.java
+++ birdsong/src/com/example/birdsong/SMSReceiver.java
@@ -3,51 +3,38 @@ package com.example.birdsong;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentReceiver;
+import android.content.BroadcastReceiver;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.gsm.SmsMessage;
 import android.util.Log;
  
-public class SMSReceiver extends IntentReceiver {
-        /** TAG used for Debug-Logging */
-        private static final String LOG_TAG = "SMSReceiver";
- 
-        /** The Action fired by the Android-System when a SMS was received.
-         * We are using the Default Package-Visibility */
-        private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
-        // @Override
-        public void onReceiveIntent(Context context, Intent intent) {
-                if (intent.getAction().equals(ACTION)) {
-                        // if(message starts with SMStretcher recognize BYTE)
-                        StringBuilder sb = new StringBuilder();
-                       
-                        /* The SMS-Messages are 'hiding' within the extras of the Intent. */
-                        Bundle bundle = intent.getExtras();
-                        if (bundle != null) {
-                                /* Get all messages contained in the Intent*/
-                                SmsMessage[] messages =
-                                        Telephony.Sms.Intents.getMessagesFromIntent(intent);
-                               
-                                /* Feed the StringBuilder with all Messages found. */
-                                for (SmsMessage currentMessage : messages){
-                                        sb.append("Received compressed SMSnFrom: ");
-                                        /* Sender-Number */
-                                        sb.append(currentMessage.getDisplayOriginatingAddress());
-                                        sb.append("n----Message----n");
-                                        /* Actual Message-Content */
-                                        sb.append(currentMessage.getDisplayMessageBody());
-                                }
-                        }
-                        /* Logger Debug-Output */
-                        Log.i(LOG_TAG, "[SMSApp] onReceiveIntent: " + sb);
- 
-                        /* Show the Notification containing the Message. */
-                        Toast.makeText(context, sb.toString(), Toast.LENGTH_LONG).show();
-                        /* Start the Main-Activity */
-                        Intent i = new Intent(context, SMSActivity.class);
-                        i.setLaunchFlags(Intent.NEW_TASK_LAUNCH);
-                        context.startActivity(i);
+public class SmsListener extends BroadcastReceiver{
+
+    private SharedPreferences preferences;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+            SmsMessage[] msgs = null;
+            String msg_from;
+            if (bundle != null){
+                //---retrieve the SMS message received---
+                try{
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for(int i=0; i<msgs.length; i++){
+                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        String msgBody = msgs[i].getMessageBody();
+                    }
+                }catch(Exception e){
+//                            Log.d("Exception caught",e.getMessage());
                 }
+            }
         }
+    }
 }
