3d30311a8200cbacb1a10c853d09208ab2a63303
Peter Jacobson <peterjacobson37@gmail.com>
2014-10-26 05:44:46 +0000

    added dictionary map

--- SMSReceiver/src/com/receiver/SMSReceiver.java
+++ SMSReceiver/src/com/receiver/SMSReceiver.java
@@ -4,6 +4,10 @@
  */
 package com.receiver;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,27 +31,74 @@ public class SMSReceiver extends BroadcastReceiver {
      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
+        
+    	//DICTIONARIES
+    	// create map to store
+    	//Map<String, List<String>> modMap = new HashMap<String, List<String>>();
+    	HashMap<Integer, String> modMap = new HashMap<Integer, String>();
+    	// put values into map
+    	modMap.put(0, "birds1");
+    	modMap.put(1, "");
+    	modMap.put(2, "");
+    	modMap.put(3, "");
+    	modMap.put(4, "");
+    	modMap.put(5, "");
+    	modMap.put(6, "");
+    	modMap.put(7, "");
+    	modMap.put(8, "");
+    	modMap.put(9, "");
+    	modMap.put(10, "");
+    	modMap.put(11, "");
+    	modMap.put(12, "");
+    	modMap.put(13, "");
+    	modMap.put(14, "");
 
+    	// create map to store
+
+    	HashMap<Integer, String> customMap = new HashMap<Integer, String>();
+
+    	// put values into map
+    	customMap.put(33, "birds1"); // !
+    	customMap.put(46, ""); // .
+    	customMap.put(63, ""); // ?
+    	customMap.put(32, ""); // " "
+    	customMap.put(126, ""); // ~
+    	customMap.put(48, ""); // 0
+    	customMap.put(49, ""); // 1
+    	customMap.put(50, ""); // ...
+    	customMap.put(51, ""); // ...
+    	customMap.put(52, ""); // ...
+    	customMap.put(53, ""); // ...
+    	customMap.put(54, ""); // ...
+    	customMap.put(55, ""); // ...
+    	customMap.put(56, ""); // ...
+    	customMap.put(57, ""); // 9
+    	
+        
+        // retrieves sms object and puts into string
+    	Bundle bundle = intent.getExtras();
         Object messages[] = (Object[]) bundle.get("pdus");
         SmsMessage smsMessage[] = new SmsMessage[messages.length];
         for (int n = 0; n < messages.length; n++) {
             smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
-            //smsIntegers[n] = Character.getNumericValue(smsMessage[n]);
         }
         String messageString = smsMessage[0].getMessageBody();
+        // converting sms body string into utf-8 character array
         char[] messageCharacters = messageString.toCharArray();
         int[] messageIntegers= new int[messageCharacters.length];
         for (int n = 0; n < messageCharacters.length; n++) {
         	messageIntegers[n] = messageCharacters[n]%5;	
         }
-        for (int n = 0; n < messageCharacters.length; n++) {
-        	//if me
-        	String intString = messageIntegers.toString();
-        }
+
+        // handling audio files
         int resourceid = 0;
-		try {
-			resourceid = R.raw.class.getField("birds" + messageIntegers[0]).getInt(null);
+        try {
+        	for (int n = 0; n < messageIntegers.length; n++)
+        	{
+        		resourceid = R.raw.class.getField((String)modMap.get(n)).getInt(null);
+				MediaPlayer mediaplayer = MediaPlayer.create(context, resourceid);
+				mediaplayer.start();
+        	}
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,12 +109,6 @@ public class SMSReceiver extends BroadcastReceiver {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		MediaPlayer mediaplayer = MediaPlayer.create(context, resourceid);
-		mediaplayer.start();
-		mediaplayer = MediaPlayer.create(context, R.raw.birds1);
-		//mediaplayer.start();
-		mediaplayer = MediaPlayer.create(context, R.raw.birds2);
-		//mediaplayer.start();
         
 		Toast toast = Toast.makeText(context, "Array" + messageIntegers[0] + smsMessage[0].getMessageBody(), Toast.LENGTH_LONG);
         toast.show();
